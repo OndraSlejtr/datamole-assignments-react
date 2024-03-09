14ceb7b8835f5e5ce7e5eb9e0d21f9396c6af5a4
Ondrej Slejtr <o.slejtr@gmail.com>
2024-03-09 21:02:33 +0100

    feat(client): add row alignment and spacing in footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 const FooterStyled = styled.footer`
     display: flex;
+    flex-direction: row;
+    justify-content: space-around;
 
     margin-top: 15px;
     padding-top: 15px;
@@ -10,6 +12,8 @@ const FooterStyled = styled.footer`
     border-top: 1px solid;
     border-color: ${(props) => props.theme.colors.olive6};
 `;
+
+const FooterItem = styled.div``;
 
 type FooterProps = {
     todoItems?: number;
@@ -19,8 +23,8 @@ type FooterProps = {
 export const Footer = ({ todoItems = 0, doneItems = 0 }: FooterProps) => {
     return (
         <FooterStyled>
-            Todo: {todoItems}
-            Done: {todoItems}
+            <FooterItem>Todo: {todoItems}</FooterItem>
+            <FooterItem>Done: {todoItems}</FooterItem>
         </FooterStyled>
     );
 };
